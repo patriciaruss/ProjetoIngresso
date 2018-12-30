64a1981e416f39dd8320a27893872a7efcf1e1a7
Patricia Russ <pmeirelesruss@gmail.com>
2018-12-30 11:43:04 -0200

    Projeto Patricia

---
+++ ProjetoIngresso/Data.Ingresso/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Data.Ingresso
+{
+    public class Class1
+    {
+    }
+}
---
+++ ProjetoIngresso/Ingresso.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ingresso.Domain
+{
+    public class Class1
+    {
+    }
+}
