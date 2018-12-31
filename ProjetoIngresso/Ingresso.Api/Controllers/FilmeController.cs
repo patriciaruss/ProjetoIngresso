fbf5afae3ba338de23b313724307c30526195f25
Patricia Russ <pmeirelesruss@gmail.com>
2018-12-31 10:53:45 -0200

    Alterações

--- ProjetoIngresso/Ingresso.Api/Controllers/FilmeController.cs
+++ ProjetoIngresso/Ingresso.Api/Controllers/FilmeController.cs
@@ -2,41 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ingresso.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class FilmeController : ControllerBase
     {
-        // GET api/values
+        // GET: api/Filme
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET: api/Filme/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/Filme
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Filme/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
